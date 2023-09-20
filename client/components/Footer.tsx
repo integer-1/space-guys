27210164f7e03dd35753033730d7b6148b109983
Suwon Jeong <integer.won@gmail.com>
2023-09-21 11:06:12 +1200

    add css style

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <>
+    <div className="footer">
       <a href="https://nasa.gov/">Click here to learn more about Space</a>
-    </>
+    </div>
   )
 }
 
--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,8 +1,9 @@
 function Header() {
   return (
-    <>
+    <div className="header">
       <h1>The Space Guys</h1>
-    </>
+      <p>Astronomy Picture of the Day</p>
+    </div>
   )
 }
 
