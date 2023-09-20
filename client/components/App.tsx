497d28f1ff139d370a6c740a1ff8f275be0dea11
Hope Goldsack <hsgoldsack@gmail.com>
2023-09-21 10:29:16 +1200

    footer component added

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,13 @@
 import Space from './Space.tsx'
 import Header from './Header.tsx'
+import Footer from './Footer.tsx'
 
 function App() {
   return (
     <>
       <Header />
       <Space />
+      <Footer />
     </>
   )
 }
---
+++ client/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <>
+      <a href="https://nasa.gov/">Click here to learn more about Space</a>
+    </>
+  )
+}
+
+export default Footer
